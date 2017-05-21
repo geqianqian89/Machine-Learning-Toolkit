0a9c1ff7d049f51c2c973bfc7508dcd7616858a2
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2017-05-20 23:09:43 -0300

    Project reorganized and build folder created.

---
+++ build/CMakeFiles/Classification_Algorithms_System.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mateus558/Dropbox/Aprendizado de Máquinas/Classification_Algorithms_System/src/Data.cpp" "/home/mateus558/Dropbox/Aprendizado de Máquinas/build-Classification_Algorithms_System-Desktop_Qt_5_8_0_GCC_64bit-Default/CMakeFiles/Classification_Algorithms_System.dir/src/Data.cpp.o"
+  "/home/mateus558/Dropbox/Aprendizado de Máquinas/Classification_Algorithms_System/src/Kernel.cpp" "/home/mateus558/Dropbox/Aprendizado de Máquinas/build-Classification_Algorithms_System-Desktop_Qt_5_8_0_GCC_64bit-Default/CMakeFiles/Classification_Algorithms_System.dir/src/Kernel.cpp.o"
+  "/home/mateus558/Dropbox/Aprendizado de Máquinas/Classification_Algorithms_System/src/Point.cpp" "/home/mateus558/Dropbox/Aprendizado de Máquinas/build-Classification_Algorithms_System-Desktop_Qt_5_8_0_GCC_64bit-Default/CMakeFiles/Classification_Algorithms_System.dir/src/Point.cpp.o"
+  "/home/mateus558/Dropbox/Aprendizado de Máquinas/Classification_Algorithms_System/src/Statistics.cpp" "/home/mateus558/Dropbox/Aprendizado de Máquinas/build-Classification_Algorithms_System-Desktop_Qt_5_8_0_GCC_64bit-Default/CMakeFiles/Classification_Algorithms_System.dir/src/Statistics.cpp.o"
+  "/home/mateus558/Dropbox/Aprendizado de Máquinas/Classification_Algorithms_System/src/Utils.cpp" "/home/mateus558/Dropbox/Aprendizado de Máquinas/build-Classification_Algorithms_System-Desktop_Qt_5_8_0_GCC_64bit-Default/CMakeFiles/Classification_Algorithms_System.dir/src/Utils.cpp.o"
+  "/home/mateus558/Dropbox/Aprendizado de Máquinas/Classification_Algorithms_System/src/Validation.cpp" "/home/mateus558/Dropbox/Aprendizado de Máquinas/build-Classification_Algorithms_System-Desktop_Qt_5_8_0_GCC_64bit-Default/CMakeFiles/Classification_Algorithms_System.dir/src/Validation.cpp.o"
+  "/home/mateus558/Dropbox/Aprendizado de Máquinas/Classification_Algorithms_System/src/main.cpp" "/home/mateus558/Dropbox/Aprendizado de Máquinas/build-Classification_Algorithms_System-Desktop_Qt_5_8_0_GCC_64bit-Default/CMakeFiles/Classification_Algorithms_System.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Classification_Algorithms_System.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Classification_Algorithms_System.dir/src/Data.cpp.o"
+  "CMakeFiles/Classification_Algorithms_System.dir/src/Kernel.cpp.o"
+  "CMakeFiles/Classification_Algorithms_System.dir/src/Point.cpp.o"
+  "CMakeFiles/Classification_Algorithms_System.dir/src/Statistics.cpp.o"
+  "CMakeFiles/Classification_Algorithms_System.dir/src/Utils.cpp.o"
+  "CMakeFiles/Classification_Algorithms_System.dir/src/Validation.cpp.o"
+  "CMakeFiles/Classification_Algorithms_System.dir/src/main.cpp.o"
+  "Classification_Algorithms_System.pdb"
+  "Classification_Algorithms_System"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Classification_Algorithms_System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
