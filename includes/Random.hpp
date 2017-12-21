d18e68971876159eb1fd82aa34c609bcfbb4b7ed
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2017-12-21 19:17:41 -0200

    Random utility improved.

--- includes/Random.hpp
+++ includes/Random.hpp
@@ -2,14 +2,38 @@
 // Created by mateus558 on 25/08/17.
 //
 
-#ifndef DONKEY_KONG_TRAB_RANDOM_H
-#define DONKEY_KONG_TRAB_RANDOM_H
+#ifndef RANDOM_H_INCLUDED
+#define RANDOM_H_INCLUDED
 
+#include <random>
+#include <functional>
 
 namespace Random {
-    void init();
-    int intInRange(int low, int high);
-    float floatInRange(float low, float high);
+    std::mt19937 m_gen;
+	unsigned int m_seed;
+	
+    auto init() {
+    	m_seed = std::random_device{} ();
+        m_gen.seed(m_seed);       
+        
+        return m_seed; 
+    }
+
+    int intInRange(int low, int high) {
+        std::uniform_int_distribution<int> dist(low, high);
+
+        return dist(m_gen);
+    }
+
+    float floatInRange(float low, float high) {
+        std::uniform_real_distribution<float> dist(low, high);
+		
+        return dist(m_gen);
+    }
+    
+    auto getSeed(){
+    	return m_seed;
+    }   
 }
 
 
